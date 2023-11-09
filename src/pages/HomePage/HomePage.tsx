635b93efa1f42c7cfcab7a1cffde2a96fd413fb5
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-11-09 01:22:00 -0300

    :construction: create grid sections

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
-// type Props = {}
 
 import Header from "../../components/Header/Header";
+import ProjectSideBar from "../../components/ProjectSideBar/ProjectSideBar";
+import "./Home.scss"
 
 const HomePage = () => {
   return (
-    <>
+    <div className="container">
       <Header/>
-    </>
+      <ProjectSideBar/>
+    </div>
   );
 };
 
