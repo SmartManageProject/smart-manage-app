d9975549866e97007644e25c80f9d26e51e84af7
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-11-09 00:45:01 -0300

    :lipstick: header scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { useUserLogged } from "../../Context/GetUser/useGetUser"
-
+import styles from './Header.module.scss'
 
 const Header = () => {
   
@@ -8,12 +8,12 @@ const Header = () => {
   const userRole = user.getRole();
 
   return (
-    <header>
-      <img src="./logoSmartManage.png" alt="Logo Smart Manage" />
-      <div>
-        <div>
-          <p>{userName}</p>
-          <p>{userRole}</p>
+    <header className={styles['header-container']}>
+      <img className={styles.logo} src="./logoMenorSmartManage.png" alt="Logo Smart Manage" />
+      <div className={styles.userContainer}>
+        <div className={styles.userInformation}>
+          <p className={styles.nome}>{userName}</p>
+          <p className={styles.role}>{userRole}</p>
         </div>
         <img src="./userIcon.png" alt="Icone de usuário"/>
       </div>
